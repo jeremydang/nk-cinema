2be967efc1edd6171ef17cc8477d13f4586581e6
JoeNg93 <ntuandung93@gmail.com>
2017-12-16 16:30:54 +0200

    Finish Choose Seat Page

---
+++ js/choose_seat_page.js
@@ -0,0 +1,62 @@
+const seatRows = ['F', 'E', 'D', 'C', 'B', 'A'];
+
+const seatNumbers = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  [14, 15, 16, 17]
+];
+
+const seatProperties = {};
+seatRows.forEach(seatRow => {
+  seatProperties[seatRow] = [];
+});
+
+const availableColor = '#531EC6';
+const takenColor = '#544A54';
+const selectedColor = '#FF1F2F';
+
+for (let i = 0; i < 18; i++) {
+  Object.keys(seatProperties).forEach(key => {
+    const random = Math.round(Math.random());
+    seatProperties[key].push({
+      color: random === 0 ? takenColor : availableColor
+    });
+  });
+}
+
+const chooseSetPage = new Vue({
+  el: '#chooseSeatPage',
+  data: {
+    seatRows: seatRows,
+    seatNumbers: seatNumbers,
+    seatProperties: seatProperties,
+    currentBackgroundPoster: "url('/assets/images/wonder_woman_poster.jpg')",
+    selectedSeats: []
+  },
+  computed: {
+    selectedSeatsCount: function() {
+      return Object.keys(seatProperties).reduce(
+        (totalCount, currentRow) =>
+          totalCount +
+          seatProperties[currentRow].filter(
+            seatNumber => seatNumber.color === selectedColor
+          ).length,
+        0
+      );
+    }
+  },
+  methods: {
+    onClickSelectSeat: function(seatRow, seatNumber) {
+      const { color } = this.seatProperties[seatRow][seatNumber];
+      if (color !== takenColor) {
+        if (color === availableColor) {
+          this.seatProperties[seatRow][seatNumber].color = selectedColor;
+          this.selectedSeats.push(seatRow + seatNumber);
+        } else {
+          this.seatProperties[seatRow][seatNumber].color = availableColor;
+          this.selectedSeats = this.selectedSeats.filter(seat => seat !== seatRow + seatNumber);
+        }
+      }
+    }
+  }
+});
