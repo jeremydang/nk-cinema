d09ff94935bee7e8ef59591c25e3784a7153e6a8
Jeremy <dang.np.thanh@gmail.com>
2017-12-20 03:04:13 +0200

    Add animation checkout page

--- js/checkout_page.js
+++ js/checkout_page.js
@@ -1,3 +1,5 @@
+const tl = new TimelineMax();
+
 const checkoutPage = new Vue({
   el: '#checkoutPage',
   data: {
@@ -10,7 +12,12 @@ const checkoutPage = new Vue({
       count: 0,
       totalPrice: 0
     },
-    totalPrice: 42
+    totalPrice: 42,
+    clientName: '',
+    clientEmail: '',
+    clientPhoneNumber: '',
+    plusBtnClick: false,
+    minusBtnClick: false,
   },
   methods: {
     onClickPopcorn: function() {
@@ -20,25 +27,44 @@ const checkoutPage = new Vue({
       this.selectedFood = 'Cola';
     },
     onClickAdd: function() {
+      this.plusBtnClick = true;
+      setTimeout( () => {this.plusBtnClick = false}, 500 )
       if (this.selectedFood === 'Popcorn') {
         this.popCorn.count += 1;
         const newPrice = 6.5 * this.popCorn.count;
         this.animatePrice(this.popCorn, newPrice);
         this.recalculateTotalPrice(newPrice, this.cola.totalPrice);
+        const { popCornImg } = this.$refs;
+        tl.to(popCornImg, 0.20,
+        { y: -25, ease:Power4.easeOut })
+        .to(popCornImg, 0.3,
+        { y: 0, ease:Bounce.easeOut });
       } else {
         this.cola.count += 1;
         const newPrice = 4.5 * this.cola.count;
         this.animatePrice(this.cola, newPrice);
         this.recalculateTotalPrice(this.popCorn.totalPrice, newPrice);
+        const { colaImg } = this.$refs;
+        tl.to(colaImg, 0.20,
+        { y: -25, ease:Power4.easeOut })
+        .to(colaImg, 0.3,
+        { y: 0, ease:Bounce.easeOut });
       }
     },
     onClickReduce: function() {
+      this.minusBtnClick = true;
+      setTimeout( () => {this.minusBtnClick = false}, 500 )
       if (this.selectedFood === 'Popcorn') {
         if (this.popCorn.count > 0) {
           this.popCorn.count -= 1;
           const newPrice = 6.5 * this.popCorn.count;
           this.animatePrice(this.popCorn, newPrice);
           this.recalculateTotalPrice(newPrice, this.cola.totalPrice);
+          const { popCornImg } = this.$refs;
+          tl.to(popCornImg, 0.2,
+          { y: 8, scaleX: 1.1, scaleY: 0.9, ease:Power4.easeOut })
+          .to(popCornImg, 0.2,
+          { y: 0, scaleX: 1, scaleY: 1, ease:Bounce.easeOut });
         }
       } else {
         if (this.cola.count > 0) {
@@ -46,6 +72,11 @@ const checkoutPage = new Vue({
           const newPrice = 4.5 * this.cola.count;
           this.animatePrice(this.cola, newPrice);
           this.recalculateTotalPrice(this.popCorn.totalPrice, newPrice);
+          const { colaImg } = this.$refs;
+          tl.to(colaImg, 0.2,
+          { y: 8, scaleX: 1.1, scaleY: 0.9, ease:Power4.easeOut })
+          .to(colaImg, 0.2,
+          { y: 0, scaleX: 1, scaleY: 1, ease:Bounce.easeOut });
         }
       }
     },
